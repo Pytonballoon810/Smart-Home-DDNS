637ef381161418b69c188e1a0fc836b0c794f437
Philipp Hofmann <philipp@hofmann-ebs.de>
2024-05-20 21:50:18 +0200

    chore: Update main.rs to bind to all available IP addresses
    The code change modifies the `main.rs` file to bind the web server to all available IP addresses by setting the IP address to `0.0.0.0`. This change allows the server to listen on all network interfaces instead of just the loopback address.

--- ddns-rust/src/main.rs
+++ ddns-rust/src/main.rs
@@ -191,7 +191,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // web server setup
     let port = env::var("PORT").unwrap_or(String::from("12080"));
     let server_addr = SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
         port.parse().unwrap(),
     );
 
